fb0c74e7d4a399797e9bd906cc40f4cfb08ec740
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-08-04 19:01:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+def print_hi(name):
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element(By.NAME, "q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    driver.close()
+
+    parser = argparse.ArgumentParser(description="Visit openclassroom and look for something")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
