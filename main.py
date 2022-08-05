02f39cdce71e40008d46a39652c884e33f00aa1f
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-08-05 17:25:26 +0200

    ajout du gitignore et chgmt pour aller sur le site amazon

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import argparse
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
+def go_look_for_book(name):
 
-def print_hi(name):
+    driver = webdriver.Chrome()
+    driver.get("https://books.google.com/")
+    assert "Google" in driver.title
+    elem = driver.find_element(By.ID, "oc-search-input")  # We select the schearch bar
+    elem.clear()
+    elem.send_keys(name)
+    elem.send_keys(Keys.RETURN)
+
+    assert "No results found." not in driver.page_source
+
+    driver.close()
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome()
-    driver.get("http://www.amazon.fr")
-    assert "Amazon" in driver.title
-    elem = driver.find_element(By.NAME, "field-keywords")  # We select the schearch bar
-    elem.clear()
-    elem.send_keys("vélo")
-    elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
-    #If there is some results so we
-    driver.close()
+    go_look_for_book("Le parfum")
+    # If there is some results so we store it in variable and will display it on the cli below
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
