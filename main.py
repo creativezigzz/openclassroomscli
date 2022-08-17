0dd909427466f2e35b961f0c72892fe3a5f921e9
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-08-17 19:23:34 +0200

    ajout de la fonction get_googlemaps() qui va rechercher l'adresse du client, et imprimer le trajet jusqu'a lui

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import mysql.connector as mc
 from mysql.connector import errorcode
 import argparse
-import pickle
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
@@ -24,8 +22,47 @@ import time
 #     time.sleep(1)
 #     pickle.dump(driver.get_cookies(), open("pickle.pkl", "wb"))
 #     driver.close()
+def get_adresse(client):
+    address = str(client[2]) + ' ' + str(client[3])
+    return address
 
-# def get_googlemaps()
+
+def get_googlemaps(client):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get("https://google.be/maps")
+    # Laisse le temps a la page de charger
+    # Clique sur accepter quand la popup viens
+    popup = driver.find_element(By.XPATH,
+                                "/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button")
+    popup.click()
+
+    # Attends que ça charge
+    time.sleep(2)
+    # Cherche la barre de recherche
+    search_bar = driver.find_element(By.ID, "searchboxinput")
+    # Adresse
+    search_bar.send_keys(get_adresse(client))
+    search_bar.send_keys(Keys.RETURN)
+    # Fais l'itinéraire avec l'Ephec
+    itin = driver.find_element(By.XPATH,
+                               "//*[@id=\"QA0Szd\"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button")
+    itin.click()
+    time.sleep(2)
+    search_bar_iti = driver.find_element(By.XPATH, "//*[@id=\"sb_ifc51\"]/input")
+    search_bar_iti.send_keys("Ephec LLN")
+    driver.find_element(By.XPATH, "//*[@id=\"directions-searchbox-0\"]/button[1]").click()
+    time.sleep(2)
+    # Prend le temps que ça mettrait pour arriver jusqu'au client depuis l'Ephec
+    temps = driver.find_element(By.XPATH, "//*[@id=\"section-directions-trip-0\"]/div[1]")
+    temps.click()
+    time.sleep(2)
+    plan = driver.find_element(By.XPATH,"//*[@id=\"QA0Szd\"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]")
+    plan.screenshot(f"./itinéraire_to_{str(client[1])}.png")
+    # Ferme le naviguateur
+    driver.close()
+
+
 def find_to_db(db_user, db_password, client_name, db_name="test"):
     try:
         cnx = mc.connect(user=db_user,
@@ -88,7 +125,7 @@ def many_client(many):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Look for a client in the database and open a browser with "
-                    "google maps and show his position base on his address",
+                    "google maps and create a png with the directions to go to his house",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     requiredNamed = parser.add_argument_group('required named arguments')
     parser.add_argument("-c", "--client", help="Name of the client")
@@ -96,12 +133,11 @@ if __name__ == '__main__':
     requiredNamed.add_argument("-u", "--user", help="Username for access of the database", required=True)
     parser.add_argument("-dbn", "--dbname", help="Name of the database you try to access , default is test",
                         default="test")
-    args = parser.parse_args(['-h'])
+    args = parser.parse_args()
 
     result = find_to_db(args.user, args.password, [args.client], args.dbname)
-    # for x in result:
-    #    print(x)
+
     # Case of many clients with the same name, ask for the client number.
     client_unique = many_client(result)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # now open a window with selenium and search for Google Maps
+    get_googlemaps(client_unique)
