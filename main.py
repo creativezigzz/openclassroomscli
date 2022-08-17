d19851c5385cc683781925be233753b4093788c4
creativezigzz <64020049+creativezigzz@users.noreply.github.com>
2022-08-17 15:55:13 +0200

    interraction avec le client au cas ou plusieurs client

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import time
 #     pickle.dump(driver.get_cookies(), open("pickle.pkl", "wb"))
 #     driver.close()
 
-
+# def get_googlemaps()
 def find_to_db(db_user, db_password, client_name, db_name="test"):
     try:
         cnx = mc.connect(user=db_user,
@@ -73,9 +73,31 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     result = find_to_db(args.user, args.password, [args.client], args.dbname)
+    # for x in result:
+    #    print(x)
+# Case of many clients with the same name, ask for the client number.
+    if len(result) > 1:
+        print("There is more than one client with that name \n, please provide your unique client number\n")
 
-    webdriver.Chrome
-    for x in result:
-        print(x)
+        client = ()
+        ok = 0
+        while ok < 1:
+            ncli = input("What is your client number ?\n")
+            for x in result:
+                if ncli.upper() == x[0].upper():
+                    client = x
+                    ok = 1
+            if not ok:
+                print("There is no such client number for the name you provide\n")
+                again = 'a'
+                while again != 'y' or 'n':
+                    again = input("Would you like to retype ? press y for yes or n for no\n")
+                    if again == 'n':
+                        print("Please search for another name and come back")
+                        exit(1)
 
+    else:
+        client = result[0]
+
+    print(client)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
